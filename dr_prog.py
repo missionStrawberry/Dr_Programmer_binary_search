eecaf006c6f15da6bef82d7d68040e323ac73c09
missionStrawberry <annam7245@sd61learn.ca>
2021-03-31 20:52:34 -0700

    finish

--- dr_prog.py
+++ dr_prog.py
@@ -1,24 +1,3 @@
-def bin_search(arr, high, low, key):
-	while low <= high:
-		mid = (low + high) / 2
-		if arr[mid] < key:
-			low = mid + 1
-		elif a[mid] > key:
-			high = mid - 1
-		else:
-			return mid
-		return -1
-
-def query_a(q):
-	Z = int(q[0])
-	XL = int(q[2])
-	XR = int(q[3])
-	
-	min = bin_search(x_pts, N - 1, 0, XL)
-	max = bin_search(x_pts, N - 1, 0, XR)
-	
-	
-
 inp = input()
 in_ar = inp.split(" ")
 N = int(in_ar[0])
@@ -31,7 +10,6 @@ x_pts = []
 for i in range(N):
 	x_pts.append(int(in_ar2[i]))
 x_pts.sort()
-print(x_pts)
 	
 inp3 = input()
 in_ar3 = inp3.split(" ")
@@ -39,10 +17,39 @@ y_pts = []
 for i in range(M):
 	y_pts.append(int(in_ar3[i]))	
 y_pts.sort()
-print(y_pts)
 
 for i in range(Q):
-	inp_q = input()
-	in_arq = inp_q.split(" ")
+    inp_q = input()
+    q = inp_q.split(" ")
+
+
+    Z = int(q[0])
+    XL = 0
+    XR = 10**13
+    YL = 0
+    YR = 10**13
+
+    
+    if q[1] == "A" or q[1] == "C":
+        XL = int(q[2])
+        XR = int(q[3])
+        
+    if q[1] == "C":
+        YL = int(q[4])
+        YR = int(q[5])
+    elif q[1] == "B":
+        YL = int(q[2])
+        YR = int(q[3])
+
+    
+
+    counter = 0
+    for j in x_pts:
+        if j >= XL and j <= XR:
+            for l in y_pts:
+                if (j + l) <= Z and l >= YL and l <= YR:
+                    counter += 1
+        
+    print(counter)
 	
 	
